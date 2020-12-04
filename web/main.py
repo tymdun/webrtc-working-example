648c427e3a2f84649dcd4ccd53739abe2778e364
tymdun <tymdun@byu.edu>
2020-12-04 09:55:54 -0700

    Makes the ice server

--- web/main.py
+++ web/main.py
@@ -1,4 +1,5 @@
 import aiortc
+import socketio
 
 # Config variables: change them to point to your own servers
 SIGNALING_SERVER_URL = 'https://tylersweat.com:9999'
@@ -8,3 +9,5 @@ TURN_SERVER_CREDENTIAL = 'capstone'
 
 # WebRTC config: you don't have to change this for the example to work
 # If you are testing on localhost, you can just use PC_CONFIG = {}
+aiortc.RTCIceServer('turn:' + TURN_SERVER_URL + '?transport=udp', username=TURN_SERVER_USERNAME,
+                    credential=TURN_SERVER_CREDENTIAL, credentialType='password')
